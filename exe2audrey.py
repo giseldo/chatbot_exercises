8379f6dcad69170b9dd0bc40ea15a5b679d0e93a
artucavalcantej <acj@ic.ufal.br>
2019-10-24 18:46:55 -0300

    resposta

--- exe2audrey.py
+++ exe2audrey.py
@@ -23,11 +23,11 @@ def respond(message):
 # Define a function that sends a message to the bot: send_message
 def send_message(message):
     # Print user_template including the user_message
-    print(user_template.format(user_message))
+    print(user_template.format(message))
     # Get the bot's response to the message
     response = respond(message)
     # Print the bot template including the bot's response.
-    print(bot_template.format(bot_message))
+    print(bot_template.format(response))
 
 
 # Send a message to the bot
