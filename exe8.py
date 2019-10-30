6e014372bfc09c16a79ec1934b65ec69155dd2a8
giseldo <giseldo@gmail.com>
2019-10-29 22:53:02 -0300

    correcao apontado por audrey, o codigo estava com erro, corrigido.

--- exe8.py
+++ exe8.py
@@ -6,11 +6,11 @@
 # appropriate response to be handled by send_message().
 
 # Instructions
-# Get a response and phrase by calling match_rule() with the rules dictionary and message.
-# Check if the response is a template by seeing if it includes the string '{0}'. If it does:
-# Use the replace_pronouns() function on phrase.
-# Include the phrase by using .format() on response and overriding the value of response.
-# Hit 'Submit Answer' to see how the bot responds to the provided messages!
+# 1. Get a response and phrase by calling match_rule() with the rules dictionary and message.
+# 2. Check if the response is a template by seeing if it includes the string '{0}'. If it does: Use the replace_pronouns() function on phrase.
+# 3. Include the phrase by using .format() on response and overriding the value of response.
+# 4. Hit 'Submit Answer' to see how the bot responds to the provided messages!
+
 import re
 import random
 
@@ -46,7 +46,7 @@ def match_rule(rules, message):
             if '{0}' in response:
                 phrase = match.group(1)
     # Return the response and phrase
-    return response.format(phrase)
+    return response, phrase
 
 
 # Define replace_pronouns()
@@ -80,12 +80,12 @@ def send_message(message):
 # Define respond()
 def respond(message):
     # Call match_rule
-    ____, ____ = ____
+    ____, ____ = ____(____,____)
     if '{0}' in response:
         # Replace the pronouns in the phrase
-        phrase = ____
+        phrase = ____(____)
         # Include the phrase in the response
-        response = ____
+        response = ____.____(____)
     return response
 
 # Send the messages
